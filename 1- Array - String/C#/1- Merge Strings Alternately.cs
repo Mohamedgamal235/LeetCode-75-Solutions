4bba46e19e648342cae473010f28adac98905779
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-17 19:32:57 +0200

    Problem Solution

--- 1- Array - String/C#/1- Merge Strings Alternately.cs
+++ 1- Array - String/C#/1- Merge Strings Alternately.cs
@@ -1,5 +1,25 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        
+        string ans = "" ; 
+
+        int i = 0 , j = 0 ; 
+        while(i < word1.Length && j < word2.Length){
+            ans += word1[i] ; 
+            ans += word2[j] ;
+            i++ ;
+            j++ ; 
+        }
+
+        while(i < word1.Length){
+            ans += word1[i] ; 
+            i++ ; 
+        }
+
+        while(j < word2.Length){
+            ans += word2[j] ; 
+            j++ ; 
+        }
+
+        return ans ; 
     }
 }
