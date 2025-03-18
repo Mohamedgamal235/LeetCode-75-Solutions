51fcb34c210c4715f7085cd3828f76c3acc317ee
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-18 18:01:14 +0200

    Problem Solution

--- 1- Array - String/C#/3- Kids With the Greatest Number of Candies.cs
+++ 1- Array - String/C#/3- Kids With the Greatest Number of Candies.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public IList<bool> KidsWithCandies(int[] candies, int extraCandies) {
+        int maxi = candies.Max() ; 
+
+        List<bool> ans = new List<bool>() ;
         
+        foreach(int x in candies){
+            if (maxi <= x + extraCandies)
+                ans.Add(true) ; 
+            else 
+                ans.Add(false) ; 
+        }
+
+        return ans ; 
     }
 }
