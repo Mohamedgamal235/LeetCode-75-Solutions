36632e83b864247d7373e52437dcb08de72fc112
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-20 07:55:32 +0200

    Problem Solution

--- 1- Array - String/C#/6- Reverse Words in a String.cs
+++ 1- Array - String/C#/6- Reverse Words in a String.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public string ReverseWords(string s) {
-        
+        string ans = "" ; 
+        for (int i = 0 ; i < s.Length ; i++){
+            if (i + 1 < s.Length && s[i] == ' ' && s[i+1] != ' ')
+                ans += ' ' ; 
+            else if (s[i] != ' ')
+                ans += s[i] ; 
+        }
+
+        string[] str = ans.Split(" ");
+        Array.Reverse(str) ; 
+        string sss = string.Join(" " , str);
+
+        return sss.Trim(); 
     }
 }
