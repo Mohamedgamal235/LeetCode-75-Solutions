ae88f9031974f107c9b9f8a5a9f5b65d43a03ee3
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-21 06:47:30 +0200

    Problem Solution

--- 2- Two Pointers/2- Is Subsequence.cs
+++ 2- Two Pointers/2- Is Subsequence.cs
@@ -1,5 +1,15 @@
 public class Solution {
     public bool IsSubsequence(string s, string t) {
-        
+        int l = 0 , r = 0 ; 
+        while(l < s.Length && r < t.Length){
+            if (s[l] == t[r]){
+                l++ ;
+                r++ ; 
+            }
+            else 
+                r++ ; 
+        }
+
+        return l == s.Length ; 
     }
 }
