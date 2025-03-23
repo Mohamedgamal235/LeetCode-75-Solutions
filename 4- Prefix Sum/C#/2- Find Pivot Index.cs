c369c630246bdff16cc518bd42a0168f87275cd0
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 06:51:14 +0200

    Problem Solution

--- 4- Prefix Sum/C#/2- Find Pivot Index.cs
+++ 4- Prefix Sum/C#/2- Find Pivot Index.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public int PivotIndex(int[] nums) {
-        
+        int n = nums.Length ; 
+        long[] prefix = new long[n+1] ;
+        for (int i = 1 ; i <= n ; i++)
+            prefix[i] += prefix[i-1] + nums[i-1] ; 
+
+        for (int i = 1 ; i <= n ; i++){
+            long left = prefix[i] - nums[i-1] ; 
+            long right = prefix[n] - prefix[i] ; 
+            if (left == right)
+                return i - 1 ; 
+        }
+
+        return -1 ;  
     }
 }
