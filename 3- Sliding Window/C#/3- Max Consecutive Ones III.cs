4a887f3e05727ffd63fb39dae87e668d6f02e35c
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 07:11:36 +0200

    Problem Solution

--- 3- Sliding Window/C#/3- Max Consecutive Ones III.cs
+++ 3- Sliding Window/C#/3- Max Consecutive Ones III.cs
@@ -1,5 +1,23 @@
 public class Solution {
     public int LongestOnes(int[] nums, int k) {
-        
+        int l = 0 , r = 0 , zeros = 0 ; 
+        int n = nums.Length ; 
+        int ans = 0 ; 
+        while(r < n){
+            if (nums[r] == 0)
+                zeros++ ; 
+            
+            while(l <= r && zeros > k){
+                if (nums[l] == 0)
+                    zeros-- ; 
+                l++ ; 
+            }
+
+            ans = Math.Max(ans , r - l + 1);
+
+            r++ ; 
+        }
+
+        return ans ; 
     }
 }
