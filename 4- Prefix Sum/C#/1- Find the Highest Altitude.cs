ca69561e5804ef05b432b6388f0d007e448bbd29
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 06:49:42 +0200

    Problem Solution

--- 4- Prefix Sum/C#/1- Find the Highest Altitude.cs
+++ 4- Prefix Sum/C#/1- Find the Highest Altitude.cs
@@ -1,5 +1,11 @@
 public class Solution {
     public int LargestAltitude(int[] gain) {
+        int maxi = Math.Max(gain[0] , 0) ; 
         
+        for (int i = 1 ; i < gain.Length ; i++){
+            gain[i] += gain[i-1] ; 
+            maxi = Math.Max(gain[i] , maxi );
+        }
+        return maxi ; 
     }
 }
