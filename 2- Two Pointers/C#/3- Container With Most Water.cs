47d172904af1f571ae3214653a4f6893d1a28db0
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 05:15:22 +0200

    Problem Solution

--- 2- Two Pointers/C#/3- Container With Most Water.cs
+++ 2- Two Pointers/C#/3- Container With Most Water.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public int MaxArea(int[] height) {
-        
+        int n = height.Length ; 
+        int l = 0 , r = n - 1 ;
+        int maxi = int.MinValue ; 
+        while(l < r){
+            int curr = Math.Min(height[l] ,height[r]) * (r - l);
+            maxi = Math.Max(maxi , curr);
+            if (height[l] > height[r])
+                r-- ;
+            else 
+                l++ ; 
+        }
+        return maxi ; 
     }
 }
