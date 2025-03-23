d4942ff740a098a5d247fe533e8906a050b245ae
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 06:19:24 +0200

    Problem Solution

---
+++ 3- Sliding Window/C#/2- Maximum Number of Vowels in a Substring of Given Length.cs
@@ -0,0 +1,9 @@
+public class Solution {
+
+    public bool isVowel(char c){
+        return c == 'a' || c == 'o' || c == 'i' || c == 'u' || c == 'e' ; 
+    }
+    public int MaxVowels(string s, int k) {
+        
+    }
+}
