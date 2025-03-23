8eb5ab8e92483ba2807d149db49dbfc0a9f38d5b
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 04:10:25 +0200

    Problem Solution

--- 1- Array - String/C#/8- Increasing Triplet Subsequence.cs
+++ 1- Array - String/C#/8- Increasing Triplet Subsequence.cs
@@ -1,5 +1,16 @@
 public class Solution {
     public bool IncreasingTriplet(int[] nums) {
-        
+        int f = int.MaxValue ; 
+        int s = int.MaxValue ;
+
+        foreach(int x in nums){
+            if (f >= x)
+                f = x ; 
+            else if (s >= x)
+                s = x ; 
+            else 
+                return true ; 
+        } 
+        return false ; 
     }
 }
