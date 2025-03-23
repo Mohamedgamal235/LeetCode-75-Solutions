284fd023eb4424a41639d7dfa74845d5a4328b57
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 07:33:48 +0200

    Problem Solution

--- 5- Hash Map - Set/C#/2- Unique Number of Occurrences.cs
+++ 5- Hash Map - Set/C#/2- Unique Number of Occurrences.cs
@@ -1,5 +1,21 @@
 public class Solution {
     public bool UniqueOccurrences(int[] arr) {
-        
+        Dictionary<int , int> freq = new Dictionary<int, int>() ; 
+
+        foreach(int x in arr){
+            if (freq.ContainsKey(x))
+                freq[x]++ ; 
+            else 
+                freq[x] = 1 ; 
+        }
+            
+        HashSet<int> st = new HashSet<int>() ; 
+        foreach(KeyValuePair<int , int> p in freq){
+            if (!st.Contains(p.Value))
+                st.Add(p.Value);
+            else 
+                return false ; 
+        }
+        return true ; 
     }
 }
