8094b51aada070d5a4dae190834ff8ba2247544c
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 05:43:41 +0200

    Problem Solution

--- 2- Two Pointers/C#/4- Max Number of K-Sum Pairs.cs
+++ 2- Two Pointers/C#/4- Max Number of K-Sum Pairs.cs
@@ -1,5 +1,20 @@
 public class Solution {
     public int MaxOperations(int[] nums, int k) {
-        
+        int l = 0 , r = nums.Length - 1 ; 
+        int ans = 0 ; 
+        Array.Sort(nums) ; 
+        while(l < r){
+            if (nums[l] + nums[r] > k)
+                r-- ;
+            else if (nums[l] + nums[r] < k)
+                l++ ; 
+            else {
+                ans++ ; 
+                l++ ; 
+                r-- ;
+            }
+        }
+
+        return ans ;
     }
 }
