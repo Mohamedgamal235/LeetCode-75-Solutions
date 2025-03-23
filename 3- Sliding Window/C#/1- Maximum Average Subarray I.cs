6f934f11220c2e22ddd238f6d17014950fdf83bb
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 06:16:24 +0200

    Problem Solution

--- 3- Sliding Window/C#/1- Maximum Average Subarray I.cs
+++ 3- Sliding Window/C#/1- Maximum Average Subarray I.cs
@@ -1,6 +1,15 @@
 public class Solution {
     public double FindMaxAverage(int[] nums, int k) {
         double sum = 0 , average = 0; 
-
+        for (int i = 0 ; i < k ; i++)
+            sum += nums[i] ; 
+            
+        average = Math.Round(sum / k , 5) ;
+        for (int i = k ; i < nums.Length ; i++){
+            sum -= nums[i-k];
+            sum += nums[i] ; 
+            average = Math.Max(average , Math.Round(sum / k , 5));
+        }
+        return average ; 
     }
 }
