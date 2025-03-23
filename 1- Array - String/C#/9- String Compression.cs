6f55372e180c330b42e0baf701a800a84f48b252
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 04:43:07 +0200

    Problem Solution

--- 1- Array - String/C#/9- String Compression.cs
+++ 1- Array - String/C#/9- String Compression.cs
@@ -1,5 +1,32 @@
 public class Solution {
     public int Compress(char[] chars) {
-        
+        int n = chars.Length ; 
+        if (n == 1)
+            return 1 ; 
+
+        int i = 0 ; 
+        int ans = 0 ; 
+        int j = 0 ; 
+        while(i < n){
+            char curr = chars[i] ; 
+            int cnt = 0 ; 
+
+            while(i < n && curr == chars[i]){
+                i++ ; 
+                cnt++ ; 
+            }
+
+            chars[j] = curr ; 
+            j++ ; 
+
+            if (cnt > 1){
+                string s = cnt.ToString() ; 
+                foreach(char c in s){
+                    chars[j] = c ; 
+                    j++ ; 
+                }
+            }
+        }
+        return j ; 
     }
 }
