2a13dd3d4a2f2840e5e4217469619e8064702477
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-23 07:53:00 +0200

    Problem Solution

--- 06- Stack/C#/1- Removing Stars From a String.cs
+++ 06- Stack/C#/1- Removing Stars From a String.cs
@@ -1,5 +1,17 @@
 public class Solution {
     public string RemoveStars(string s) {
-        
+        Stack<char> st = new Stack<char>();
+
+        foreach(char c in s){
+            if (c != '*')
+                st.Push(c);
+            else if (st.Count != 0 && c == '*')
+                st.Pop() ;
+        }
+        char[] ans = new char[st.Count] ; 
+        for (int i = st.Count - 1 ; i >= 0 ; i--)
+            ans[i] = st.Pop() ; 
+
+        return new string(ans); 
     }
 }
