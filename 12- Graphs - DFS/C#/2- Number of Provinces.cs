d00fa89f5ab07e17d898be175e06dcb5b6325e22
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-03-26 17:56:07 +0200

    Problem Solution

--- 12- Graphs - DFS/C#/2- Number of Provinces.cs
+++ 12- Graphs - DFS/C#/2- Number of Provinces.cs
@@ -1,5 +1,37 @@
 public class Solution {
+
+    public void DFS(int node , List<List<int>> adjList , bool[] visited){
+        visited[node] = true ; 
+        foreach(var neighbour in adjList[node]){
+            if (!visited[neighbour])
+                DFS(neighbour , adjList , visited);
+        }
+    }
     public int FindCircleNum(int[][] isConnected) {
-        
+        int n = isConnected.Length ; 
+        List<List<int>> adjList = new List<List<int>>(n);
+        bool[] visited = new bool[n] ;
+
+        for (int i = 0 ; i < n ; i++)
+            adjList.Add(new List<int>());
+
+        for (int i = 0 ; i < n ; i++){
+            for (int j = 0 ; j < n ; j++){
+                if (isConnected[i][j] == 1){
+                    adjList[i].Add(j);
+                    adjList[j].Add(i);
+                }
+            }
+        }
+
+        int ans = 0 ; 
+        for (int i = 0 ; i < n ; i++){
+            if (!visited[i]){
+                DFS(i , adjList , visited) ; 
+                ans++ ; 
+            }
+        }
+
+        return ans ; 
     }
 }
