ee806a12c22f1d530d86d4d20e4d21ca33c27089
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-14 18:38:02 +0300

    Problem Solution

---
+++ 02- Two Pointers/4- Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int l = 0, r = nums.size() - 1, ans = 0;
+        sort(nums.begin(), nums.end());
+        while (l < r) {
+            if (nums[l] + nums[r] > k) r--;
+            else if (nums[l] + nums[r] < k) l++;
+            else {
+                ans++;
+                l++;
+                r--;
+            }
+        }
+        return ans;
+    }
+};
