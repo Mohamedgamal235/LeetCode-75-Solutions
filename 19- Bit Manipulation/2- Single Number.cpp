5d16fb614cd740d2c2d88e5bb46679a4c7a6b116
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-16 03:46:31 +0300

    Problem Solution

---
+++ 19- Bit Manipulation/2- Single Number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if (nums.size() == 1) return nums[0];
+
+        // if use XOR :
+        // 0 ^ 0 -> 0
+        // 1 ^ 1 -> 0
+        // 0 ^ 1 -> 1
+        // 1 ^ 0 -> 1
+        int ans = nums[0];
+        for (int i = 1 ; i < nums.size() ; i++)
+            ans ^= nums[i] ;
+        return ans;
+    }
+};
