22f2fdf7f9da256574fefacb4622cdbb25e1f23e
Mohamed Gamal Ali <131499327+Mohamedgamal235@users.noreply.github.com>
2025-08-17 13:52:23 +0300

    Problem Solution

---
+++ 07- Queue/2- Dota2 Senate.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> qr , qd ;
+        int n = senate.size() ;
+        for (int i = 0 ; i < n ; i++) {
+            if (senate[i] == 'R')
+                qr.push(i);
+            else
+                qd.push(i);
+        }
+
+        while (!qr.empty() && !qd.empty()) {
+            int r = qr.front(); qr.pop();
+            int d = qd.front(); qd.pop();
+            if (r < d)
+                qr.push(r + n);
+            else
+                qd.push(d + n);
+        }
+        if (qr.empty())
+            return "Dire";
+        return "Radiant";
+    }
+};
